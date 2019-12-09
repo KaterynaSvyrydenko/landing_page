41d93460c42c579ad46cf62967c2e8d0da52bb4c
Kateryna Svyrydenko <k.m.sviridenko@gmail.com>
2019-12-09 13:09:28 +0200

    clients: 	unslick slider (width < 980px)

--- build/slider.js
+++ build/slider.js
@@ -1,22 +1,22 @@
 
-$(document).ready(function(){
-    if($('.clients_slider').width() <= '1100')
+function onResize(){
+    if ($(window).width() <= '960')
+            $('.clients_slider').slick('unslick');
+    else if($(window).width() <= '1100')
         $('.clients_slider').slick({
             infinite: true,
             slidesToShow: 1 ,
             slidesToScroll: 1,
         });
     else 
-        if ($('.clients_slider').width() > '1100')
-            $('.clients_slider').slick({
-                infinite: true,
-                slidesToShow: 2,
-                slidesToScroll: 1,
-            });
-    else
-        if ($(window).width() <= '960')
-            $('.clients_slider').slick('unslick');
-});
+        $('.clients_slider').slick({
+            infinite: true,
+            slidesToShow: 2,
+            slidesToScroll: 1,
+        });
+}
+$(document).ready(onResize);
 
+$(window).resize(onResize);
 
 
